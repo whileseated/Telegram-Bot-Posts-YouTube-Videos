da5267dbf16932c6c74f9faa339d54386f2791bf
Michael Murphy <mdm2022@MDM-AIR.attlocal.net>
2023-05-05 10:58:49 -0400

    removed --postnow flag functionality

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ from config import TOKEN, CHANNEL_NAME
 import logging
 
 def post_link(bot: Bot) -> None:
-    logging.debug('post_link function called')  
+    logging.debug('post_link function called')
     with open('videos_to_post.txt', 'r+') as f:
         links = f.readlines()
         if links:
@@ -15,24 +15,16 @@ def post_link(bot: Bot) -> None:
             f.writelines(links)
             f.truncate()
 
-    with open('videos_posted.txt', 'a+') as f:
-        f.write(link_to_post)
+            with open('videos_posted.txt', 'a+') as f:
+                f.write(link_to_post)
 
 def main() -> None:
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--postnow', action='store_true')
-    args = parser.parse_args()
-
     print(f"TOKEN: {TOKEN}")
     print(f"CHANNEL_NAME: {CHANNEL_NAME}")
 
     bot = Bot(TOKEN)
 
-    if args.postnow:
-        post_link(bot)
-    else:
-        # add your code for running the script without the --postnow flag here
-        pass
+    post_link(bot)
 
 if __name__ == '__main__':
     main()
